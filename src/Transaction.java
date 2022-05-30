9c67469cdd18b915234bbc2aa2700881a7fb9a2e
DedRec <seif.kholy@gmail.com>
2022-05-30 12:41:02 +0200

    Third Commit

---
+++ src/Transaction.java
@@ -0,0 +1,54 @@
+public class Transaction {
+    private String type;
+    private String time;
+    private double amount;
+    private Account account;
+    private Account transferAccount;
+
+
+    public Transaction(String type, double amount, Account account)
+    {
+        this.type = type;
+        this.time = time;
+        this.account = account;
+        this.amount = amount;
+        checkType();
+    }
+    public Transaction(String type, double amount, Account account,Account transferAccount)
+    {
+        this.type = type;
+        this.time = time;
+        this.account = account;
+        this.transferAccount = transferAccount;
+        this.amount = amount;
+        checkType();
+    }
+
+    public void checkType(){
+        if(type == "Withdraw"){
+            withdraw();
+        }
+        else if(type == "Deposit"){
+            deposit();
+        }
+        else if(type == "Transfer"){
+            transferMoney();
+        }
+    }
+
+    public void withdraw()
+    {
+        account.updateBalance(-amount);
+    }
+
+    public void deposit(){
+        account.updateBalance(amount);
+    }
+
+    public void transferMoney(){
+        account.updateBalance(-amount);
+        transferAccount.updateBalance(amount);
+        System.out.println(amount + " Transferred Successfully!");
+    }
+
+}
